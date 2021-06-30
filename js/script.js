e33b51e3df7101d7446181e40a93553e2b5f943f
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-06-29 22:51:07 -0400

    update styles / update js
    added another image, added a class to toggle the animations for the images

--- js/script.js
+++ js/script.js
@@ -3,43 +3,42 @@ const NEXT = document.getElementById("next");
 
 let slideIndex = 1;
 
-
 const showSlide = (n) => {
     let slides = document.getElementsByClassName("slide__img");
-    let infoBoxes = document.getElementsByClassName("info-box")
+    let infoBoxes = document.getElementsByClassName("info-box");
 
-    if(n > slides.length) {
-        slideIndex = 1
+    if (n > slides.length) {
+        slideIndex = 1;
     }
 
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slides.length;
     }
 
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"
+        slides[i].style.visibility = "hidden";
+        slides[i].classList.remove("active");
     }
 
     for (let i = 0; i < infoBoxes.length; i++) {
-        infoBoxes[i].style.display = "none"
+        infoBoxes[i].style.display = "none";
     }
 
-
-    slides[slideIndex - 1].style.display = "block"
-    infoBoxes[slideIndex - 1].style.display = "block"
-    
-}
+    slides[slideIndex - 1].style.visibility = "visible";
+    slides[slideIndex - 1].classList.add("active");
+    infoBoxes[slideIndex - 1].style.display = "block";
+};
 
 const changeSlide = (n) => {
-    showSlide(slideIndex += n)
-}
+    showSlide((slideIndex += n));
+};
 
-showSlide(slideIndex)
+showSlide(slideIndex);
 
-PREV.addEventListener("click", ()=> {
-    changeSlide(-1)
-})
+PREV.addEventListener("click", () => {
+    changeSlide(-1);
+});
 
-NEXT.addEventListener("click", ()=> {
-    changeSlide(1)
-})
+NEXT.addEventListener("click", () => {
+    changeSlide(1);
+});
